e3b954c8793d55c7d846315679d0ca3abac256fb
polymaster16 <uili.kam8@gmail.com>
2023-08-10 10:25:22 +0100

    modified:   index.html 	new file:   public/logex2.png 	modified:   src/pages/tags/[slug].vue

--- src/pages/tags/[slug].vue
+++ src/pages/tags/[slug].vue
@@ -1,20 +1,15 @@
 <template>
     <div>
         <div v-motion-fade>
- <div style="background-image: url('https://static.wixstatic.com/media/08e322_77bc8e217a404c93a3b00c23db69ba67~mv2.gif');
-" class="herox">
+ <div :style="`background-image: url(${CreateURL(category.image)});`" class="herox">
         <div class="bler">
     <navbar></navbar>
     <p class="text-3xl text-center font-bold mt-10">
-      {{ route.params.slug }}
+      {{category.title}}
     </p>
-
-    <p class="mx-6 mt-4">
-        <v-text-field variant="solo" label="🔍 Search">
-
-        </v-text-field>
+    <p class="text-sm mx-5 my-4">
+        {{ category.description }}
     </p>
-
     </div>
     </div>
 </div>
@@ -27,10 +22,13 @@ import navbar from '../../components/navbar.vue';
 import { useMainstore } from '../../stores/mainstore';
 import {CreateURL} from '@/utils'
 import {useRoute} from 'vue-router'
+import {ref, computed} from 'vue'
 
 const route = useRoute()
 
 const mainstore = useMainstore()
+
+const category = computed(()=> {return mainstore.categories.filter(x => x.title === route.params.slug)[0]})
 
 </script>
 
